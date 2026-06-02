f582b7badf11dcc3a7436e301d711c78eb1b8e4b
Shaik Mahammed Asif <mahammedasifqwerty@gmail.com>
2026-06-02 19:05:16 +0530

    Add new allowed origin for CORS

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://prepmateai-frontend.onrender.com",
+        "https://prep-mate-ai-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
